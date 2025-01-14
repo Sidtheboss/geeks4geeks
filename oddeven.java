bec753267da2f79f700f8c4606107556f06a6228
Siddharth Venkatesh <126441692+Sidtheboss@users.noreply.github.com>
2025-01-14 20:15:35 +0530

    oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+class Solution {
+    static boolean isEven(int n) {
+        return(n%2==0);
+    }
+    public static void main(String[]args){
+        int n=20;
+        if(isEven(n)==true){
+            System.out.print("true");
+        }
+        else{
+            System.out.print("false");
+        }
+    }
+    }
+    
+      
