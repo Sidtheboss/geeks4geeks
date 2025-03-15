b667da3ef9bce17251dc931b42c3e35de9a4f8e5
Siddharth Venkatesh <126441692+Sidtheboss@users.noreply.github.com>
2025-03-15 23:50:51 +0530

    passbyreference.java

---
+++ passbyreference.java
@@ -0,0 +1,8 @@
+class Solution {
+    static int[] passedBy(int a, int b) {
+        // code here
+        int[]result = new int[2];
+        result[0]=a+1;
+        result[1]=b+2;
+        return result;
+    }
