f5e055240dee8774d73ba80cd9100ca19e3cbbda
Siddharth Venkatesh <126441692+Sidtheboss@users.noreply.github.com>
2025-03-15 23:12:12 +0530

    Data Type.java

---
+++ Data Type.java
@@ -0,0 +1,23 @@
+// User function Template for Java
+
+class Solution {
+    static int dataTypeSize(String str) {
+        // code here
+        switch(str){
+            case "Character":
+                return 2;
+                case "Integer":
+                    return 4;
+                    case "Float":
+                        return 4;
+                        case "Long":
+                            return 8;
+                            case "Double":
+                                return 8;
+                                default:
+                                return -1;
+        }
+        }
+            } 
+       
+        
